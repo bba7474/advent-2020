c3df809a637782887cbd827e14b124d25e9f4375
Ben Ashby-Wood <58435096+bba7474@users.noreply.github.com>
2020-12-02 10:26:27 +0000

    Day 2 Part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -9,23 +9,20 @@ fn main() {
     announce_answer(valid_password_count.to_string());
 }
 
-
 fn count_valid_pwds(pwd_rules: Vec<String>) -> i32 {
     let mut valid_count = 0;
 
-    for pwd in pwd_rules {
-        let splitted = split(pwd, " ".to_string());
+    for pwd_rule in pwd_rules {
+        let splitted = split(pwd_rule, " ".to_string());
 
-        let min_max = split(splitted[0].to_string(), "-".to_string());
-        let min = min_max[0].parse::<i32>().expect("not integer");
-        let max = min_max[1].parse::<i32>().expect("not integer");
+        let wanted_indexes = split(splitted[0].to_string(), "-".to_string());
+        let index_1 = wanted_indexes[0].parse::<i32>().expect("not integer") - 1;
+        let index_2 = wanted_indexes[1].parse::<i32>().expect("not integer") - 1;
 
-        let expected_letter = &splitted[1][..1];
+        let expected_letter = &splitted[1].chars().nth(0).unwrap();
         let password = &splitted[2];
 
-        let matches = password.matches(expected_letter).count();
-
-        if min <= matches as i32 && matches as i32 <= max {
+        if (password.chars().nth(index_1 as usize).unwrap() == *expected_letter) ^ (password.chars().nth(index_2 as usize).unwrap() == *expected_letter) {
             valid_count += 1;
         }
     }
@@ -62,6 +59,6 @@ mod tests {
     #[test]
     fn test_count_valid_pwds() {
         let count = count_valid_pwds(vec!["1-3 a: abcde".to_string(), "1-3 b: cdefg".to_string(), "2-9 c: ccccccccc".to_string()]);
-        assert_eq!(2, count);
+        assert_eq!(1, count);
     }
 }
