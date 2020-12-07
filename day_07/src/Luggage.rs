b98362642690dd2ef83faf76d9a025944f0cdf13
Ben Ashby-Wood <58435096+bba7474@users.noreply.github.com>
2020-12-07 21:41:06 +0000

    Day 7 Part 2

--- day_07/src/Luggage.rs
+++ day_07/src/Luggage.rs
@@ -2,11 +2,11 @@ use std::str::FromStr;
 
 pub struct Luggage {
     color: String,
-    contains: Vec<String>
+    contains: Vec<LuggageContainsRules>
 }
 
 impl Luggage {
-    pub fn get_entry(&self) -> (String, Vec<String>) {
+    pub fn get_entry(&self) -> (String, Vec<LuggageContainsRules>) {
         (self.color.clone(), self.contains.clone())
     }
 }
@@ -24,14 +24,48 @@ impl FromStr for Luggage {
 
             for s in contains_strings {
                 let end_of_color = s.rfind(" ").unwrap();
-                contains.push(&s[2..end_of_color]);
+                let color = &s[2..end_of_color].to_string();
+                let count = &s[0..1].parse::<i32>().expect("not an integer");
+                contains.push(LuggageContainsRules{
+                    color: color.to_string(),
+                    count: count.clone()
+                });
             }
         }
 
         Ok(Luggage{
             color: color.to_string(),
-            contains: contains.iter().map(|s| s.to_string()).collect()
+            contains
         })
+    }
+}
+
+pub struct LuggageContainsRules {
+    color: String,
+    count: i32
+}
+
+impl LuggageContainsRules {
+    pub fn get_color(&self) -> String {
+        self.color.clone()
+    }
+    pub fn get_count(&self) -> i32 {
+        self.count.clone()
+    }
+}
+
+impl Clone for LuggageContainsRules {
+    fn clone(&self) -> Self {
+        return LuggageContainsRules {
+            color: self.color.clone(),
+            count: self.count.clone()
+        };
+    }
+}
+
+impl PartialEq for LuggageContainsRules {
+    fn eq(&self, other: &Self) -> bool {
+        self.color == other.color && self.count == other.count
     }
 }
 
@@ -44,8 +78,9 @@ mod tests {
         let luggage1 = "mirrored gold bags contain 2 pale blue bags, 1 dark violet bag.".parse::<Luggage>().unwrap();
         assert_eq!(luggage1.color, "mirrored gold");
         assert_eq!(luggage1.contains.len(), 2);
-        assert!(luggage1.contains.contains(&"pale blue".to_string()));
-        assert!(luggage1.contains.contains(&"dark violet".to_string()));
+
+        assert!(luggage1.contains.contains(&LuggageContainsRules { color: "pale blue".to_string(), count: 2 }));
+        assert!(luggage1.contains.contains(&LuggageContainsRules { color: "dark violet".to_string(), count: 1 }));
     }
 
     #[test]
@@ -53,7 +88,7 @@ mod tests {
         let luggage1 = "mirrored gold bags contain 2 pale blue bags.".parse::<Luggage>().unwrap();
         assert_eq!(luggage1.color, "mirrored gold");
         assert_eq!(luggage1.contains.len(), 1);
-        assert!(luggage1.contains.contains(&"pale blue".to_string()));
+        assert!(luggage1.contains.contains(&LuggageContainsRules { color: "pale blue".to_string(), count: 2 }));
     }
 
     #[test]
--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::io::{BufWriter, stdout};
 
 use ferris_says::say;
-use crate::luggage::Luggage;
+use crate::luggage::{Luggage, LuggageContainsRules};
 use std::collections::HashMap;
 
 mod luggage;
@@ -10,27 +10,44 @@ mod luggage;
 fn main() {
     let input = read_input();
 
-    let luggage_rules: HashMap<String, Vec<String>> = input.iter()
+    let luggage_rules: HashMap<String, Vec<LuggageContainsRules>> = input.iter()
         .map(|s| s.parse::<Luggage>().unwrap())
         .map(|l| l.get_entry())
         .collect();
 
+    let wanted_color = "shiny gold".to_string();
+
     let bags_containing_color = luggage_rules.iter()
-        .filter(|(bag, _rule)| bag_can_contain_bag(bag.to_string(), "shiny gold".to_string(), &luggage_rules))
+        .filter(|(bag, _rule)| bag_can_contain_bag(bag.to_string(), &wanted_color, &luggage_rules))
         .count();
 
-    announce_answer(bags_containing_color.to_string());
+    let bags_contained_by = count_bags_contained_in(&wanted_color, &luggage_rules);
+
+    announce_answer(format!("{:?} different color bags can contain a {} bag", bags_containing_color, wanted_color));
+    announce_answer(format!("{:?} bags a contained within a {} bag", bags_contained_by, wanted_color));
 }
 
-fn bag_can_contain_bag(bag: String, can_contain: String, luggage_rules: &HashMap<String, Vec<String>>) -> bool {
+fn bag_can_contain_bag(bag: String, can_contain: &String, luggage_rules: &HashMap<String, Vec<LuggageContainsRules>>) -> bool {
     let rule = luggage_rules.get(&bag).unwrap();
-    if rule.contains(&can_contain) {
+    if rule.iter().any(|r| &r.get_color() == can_contain) {
         return true;
     }
     if rule.len() == 0 {
         return false;
     }
-    return rule.iter().any(|new_bag| bag_can_contain_bag(new_bag.to_string(), can_contain.clone(), luggage_rules));
+    return rule.iter().any(|new_bag| bag_can_contain_bag(new_bag.get_color(), can_contain, luggage_rules));
+}
+
+fn count_bags_contained_in(bag: &String, luggage_rules: &HashMap<String, Vec<LuggageContainsRules>>) -> i32 {
+    let rules = luggage_rules.get(bag).unwrap();
+
+    let mut num_bags = 0;
+    for rule in rules {
+        num_bags += rule.get_count(); // the bags themselves
+        num_bags += rule.get_count() * count_bags_contained_in(&rule.get_color(), luggage_rules); // the bags contained
+    }
+
+    num_bags
 }
 
 fn read_input() -> Vec<String> {
@@ -42,7 +59,7 @@ fn read_input() -> Vec<String> {
 }
 
 fn announce_answer(answer: String) {
-    let message = format!("The answer is: {}", answer).to_string();
+    let message = format!("{}", answer).to_string();
     let stdout = stdout();
     let width = message.chars().count();
 
