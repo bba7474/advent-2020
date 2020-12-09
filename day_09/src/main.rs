d72314c17264e82e3a88a1a050dd44147c101ff8
Ben Ashby-Wood <58435096+bba7474@users.noreply.github.com>
2020-12-09 09:42:39 +0000

    Day 9 Part 2

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -6,8 +6,12 @@ use itertools::iproduct;
 
 fn main() {
     let input = read_input();
-    let answer = find_number_not_matching_rule(input);
-    announce_answer(format!("{} is the first number that is not a sum of two of the previous 25 numbers", answer))
+    let invalid_number = find_number_not_matching_rule(input.clone());
+    announce_answer(format!("{} is the first number that is not a sum of two of the previous 25 numbers", invalid_number));
+    let contiguous_sum_list = get_contiguous_list_summing_to(invalid_number, input.clone());
+    let min = contiguous_sum_list.iter().min().unwrap();
+    let max = contiguous_sum_list.iter().max().unwrap();
+    announce_answer(format!("{} is the sum of the min and max of the contiguous list that sums to the invalid number {}", min + max, invalid_number));
 }
 
 fn find_number_not_matching_rule(xmas_data: Vec<i64>) -> i64 {
@@ -28,6 +32,25 @@ fn has_pair_summing_to(target: i64, to_sum: Vec<i64>) -> bool {
         .any(|(x, y)| x != y && x + y == target)
 }
 
+fn get_contiguous_list_summing_to(target: i64, xmas_data: Vec<i64>) -> Vec<i64> {
+    for i in 0..xmas_data.len() - 1 {
+        let mut sum = 0;
+        let mut c_list = Vec::new();
+        for j in i..xmas_data.len() - 1 {
+            let value = xmas_data[j];
+            sum += value;
+            c_list.push(value);
+            if sum >= target {
+                break;
+            }
+        }
+        if sum == target {
+            return c_list;
+        }
+    }
+
+    return Vec::new();
+}
 
 fn read_input() -> Vec<i64> {
     fs::read_to_string("input.txt")
