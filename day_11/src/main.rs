1087d4791b4d08680c40179dad181aa8cbedf6ba
Ben Ashby-Wood <58435096+bba7474@users.noreply.github.com>
2020-12-11 12:25:58 +0000

    Day 11 Part 2

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -8,18 +8,39 @@ use crate::seating::Seating;
 mod seating;
 
 fn main() {
-    let mut seating = read_input();
-    let mut occ_seats_count = 0;
+    let seating_plan = read_input();
+
+    let occ_seats_count_1 = get_occupied_seats_1(&mut seating_plan.clone());
+    announce_answer(format!("There are {} occupied seats when the adjacency rules stabilise", occ_seats_count_1));
+
+    let occ_seats_count_2 = get_occupied_seats_2(&mut seating_plan.clone());
+    announce_answer(format!("There are {} occupied seats when the visible rules stabilise", occ_seats_count_2));
+}
+
+fn get_occupied_seats_1(seating: &mut Seating) -> i32 {
+    let mut count_occupied_seats = 0;
     loop {
-        seating.apply_rules();
+        seating.apply_rules_1();
         let count = seating.count_occupied();
-        if count == occ_seats_count {
+        if count == count_occupied_seats {
             break;
         }
-        occ_seats_count = count;
+        count_occupied_seats = count;
     }
+    count_occupied_seats
+}
 
-    announce_answer(format!("There are {} occupied seats when the rules stabilise", occ_seats_count));
+fn get_occupied_seats_2(seating: &mut Seating) -> i32 {
+    let mut count_occupied_seats = 0;
+    loop {
+        seating.apply_rules_2();
+        let count = seating.count_occupied();
+        if count == count_occupied_seats {
+            break;
+        }
+        count_occupied_seats = count;
+    }
+    count_occupied_seats
 }
 
 fn read_input() -> Seating {
