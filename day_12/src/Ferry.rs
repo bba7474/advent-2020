fff6718fad360d7068e17370571678a1bb16d669
Ben Ashby-Wood <58435096+bba7474@users.noreply.github.com>
2020-12-12 15:48:51 +0000

    Day 12 Part 2

--- day_12/src/Ferry.rs
+++ day_12/src/Ferry.rs
@@ -2,11 +2,12 @@ use std::str::FromStr;
 
 pub struct Ferry {
     direction_facing: String,
-    current_position: (i32, i32)
+    current_position: (i32, i32),
+    waypoint_relative_position: (i32, i32),
 }
 
 impl Ferry {
-    pub fn travel(&mut self, instr: Instruction) {
+    pub fn travel_1(&mut self, instr: Instruction1) {
         if instr.direction_moved.is_some() {
             self.current_position = get_new_position(&self.direction_facing, &self.current_position, instr.direction_moved.unwrap(), instr.distance_moved.unwrap());
         }
@@ -15,15 +16,44 @@ impl Ferry {
         }
     }
 
+    pub fn travel_2(&mut self, instr: Instruction2) {
+        if instr.to_waypoint_times.is_some() {
+            self.current_position = travel_to_waypoint(&self.current_position, &self.waypoint_relative_position, instr.to_waypoint_times.unwrap());
+        }
+        if instr.waypoint_move_dir.is_some() {
+            self.waypoint_relative_position = calc_new_position(&self.waypoint_relative_position, &instr.waypoint_move_dir.unwrap(), instr.waypoint_move_dist.unwrap());
+        }
+        if instr.waypoint_rotate_dir.is_some() {
+            self.waypoint_relative_position = rotate_position(&self.waypoint_relative_position, &instr.waypoint_rotate_dir.unwrap(), instr.waypoint_rotate_angle.unwrap());
+        }
+    }
+
     pub fn calc_manhattan_distance(&self) -> i32 {
         self.current_position.0.abs() + self.current_position.1.abs()
     }
 }
 
+fn rotate_position(current_position: &(i32, i32), rotate_dir: &String, rotate_angle: i32) -> (i32, i32) {
+    let mut new_position = current_position.clone();
+
+    if rotate_angle == 180 {
+        new_position = (-new_position.0, -new_position.1);
+    } else if (rotate_dir == "R" && rotate_angle == 90) || (rotate_dir == "L" && rotate_angle == 270) {
+        new_position = (new_position.1, -new_position.0);
+    } else if (rotate_dir == "R" && rotate_angle == 270) || (rotate_dir == "L" && rotate_angle == 90) {
+        new_position = (-new_position.1, new_position.0);
+    }
+
+    new_position
+}
+
 fn get_new_position(direction_facing: &String, current_position: &(i32, i32), direction_moved: String, distance: i32) -> (i32, i32) {
     let direction_to_move = if direction_moved == "F" { direction_facing } else { &direction_moved };
+    calc_new_position(current_position, direction_to_move, distance)
+}
 
-    let mut new_position =  current_position.clone();
+fn calc_new_position(current_position: &(i32, i32), direction_to_move: &String, distance: i32) -> (i32, i32) {
+    let mut new_position = current_position.clone();
 
     if direction_to_move == "N" {
         new_position = (new_position.0, new_position.1 + distance);
@@ -54,38 +84,84 @@ fn get_new_direction_faced(current_direction: &String, direction: String, angle:
     directions[new_position_index].to_string()
 }
 
-pub fn new_ferry(direction_facing: &str) -> Ferry {
-    Ferry { direction_facing: String::from(direction_facing), current_position: (0, 0) }
+fn travel_to_waypoint(current_position: &(i32, i32), waypoint_relative_pos: &(i32, i32), times_to_waypoint: i32) -> (i32, i32) {
+    (current_position.0 + (waypoint_relative_pos.0 * times_to_waypoint), current_position.1 + (waypoint_relative_pos.1 * times_to_waypoint))
 }
 
-pub struct Instruction {
+pub fn new_ferry(direction_facing: &str) -> Ferry {
+    Ferry { direction_facing: String::from(direction_facing), current_position: (0, 0), waypoint_relative_position: (10, 1) }
+}
+
+pub struct Instruction1 {
     direction_moved: Option<String>,
     distance_moved: Option<i32>,
     direction_rotated: Option<String>,
-    angle_rotated: Option<i32>
+    angle_rotated: Option<i32>,
 }
 
-impl FromStr for Instruction {
+impl FromStr for Instruction1 {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         let instr = s.chars().nth(0).unwrap().to_string();
         let scalar = s[1..].parse::<i32>().expect("not an integer");
 
         if instr == "N" || instr == "E" || instr == "S" || instr == "W" || instr == "F" {
-            Ok(Instruction{
+            Ok(Instruction1 {
                 direction_moved: Some(instr),
                 distance_moved: Some(scalar),
                 direction_rotated: None,
-                angle_rotated: None
+                angle_rotated: None,
             })
         } else {
-            Ok(Instruction {
+            Ok(Instruction1 {
                 direction_moved: None,
                 distance_moved: None,
                 direction_rotated: Some(instr),
-                angle_rotated: Some(scalar)
+                angle_rotated: Some(scalar),
+            })
+        }
+    }
+}
+
+pub struct Instruction2 {
+    to_waypoint_times: Option<i32>,
+    waypoint_move_dir: Option<String>,
+    waypoint_move_dist: Option<i32>,
+    waypoint_rotate_dir: Option<String>,
+    waypoint_rotate_angle: Option<i32>,
+}
+
+impl FromStr for Instruction2 {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let instr = s.chars().nth(0).unwrap().to_string();
+        let scalar = s[1..].parse::<i32>().expect("not an integer");
+
+        if instr == "F" {
+            Ok(Instruction2 {
+                to_waypoint_times: Some(scalar),
+                waypoint_move_dir: None,
+                waypoint_move_dist: None,
+                waypoint_rotate_dir: None,
+                waypoint_rotate_angle: None,
+            })
+        } else if instr == "N" || instr == "E" || instr == "S" || instr == "W" {
+            Ok(Instruction2 {
+                to_waypoint_times: None,
+                waypoint_move_dir: Some(instr),
+                waypoint_move_dist: Some(scalar),
+                waypoint_rotate_dir: None,
+                waypoint_rotate_angle: None,
+            })
+        } else {
+            Ok(Instruction2 {
+                to_waypoint_times: None,
+                waypoint_move_dir: None,
+                waypoint_move_dist: None,
+                waypoint_rotate_dir: Some(instr),
+                waypoint_rotate_angle: Some(scalar),
             })
         }
     }
@@ -99,36 +175,50 @@ mod tests {
     fn test_rotate() {
         let mut ferry = new_ferry("E");
 
-        ferry.travel("R90".parse::<Instruction>().unwrap());
+        ferry.travel_1("R90".parse::<Instruction1>().unwrap());
         assert_eq!("S", ferry.direction_facing);
 
-        ferry.travel("R90".parse::<Instruction>().unwrap());
+        ferry.travel_1("R90".parse::<Instruction1>().unwrap());
         assert_eq!("W", ferry.direction_facing);
 
-        ferry.travel("R270".parse::<Instruction>().unwrap());
+        ferry.travel_1("R270".parse::<Instruction1>().unwrap());
         assert_eq!("S", ferry.direction_facing);
 
-        ferry.travel("R360".parse::<Instruction>().unwrap());
+        ferry.travel_1("R360".parse::<Instruction1>().unwrap());
         assert_eq!("S", ferry.direction_facing);
 
-        ferry.travel("L90".parse::<Instruction>().unwrap());
+        ferry.travel_1("L90".parse::<Instruction1>().unwrap());
         assert_eq!("E", ferry.direction_facing);
 
-        ferry.travel("L180".parse::<Instruction>().unwrap());
+        ferry.travel_1("L180".parse::<Instruction1>().unwrap());
         assert_eq!("W", ferry.direction_facing);
     }
 
     #[test]
-    fn test_move_ferry() {
+    fn test_move_ferry_1() {
         let input = "F10\nN3\nF7\nR90\nF11";
-        let instructions: Vec<Instruction> = input.lines().map(|s| s.parse().unwrap()).collect();
+        let instructions: Vec<Instruction1> = input.lines().map(|s| s.parse().unwrap()).collect();
 
         let mut ferry = new_ferry("E");
 
         for i in instructions {
-            ferry.travel(i);
+            ferry.travel_1(i);
         }
 
         assert_eq!(25, ferry.calc_manhattan_distance());
+    }
+
+    #[test]
+    fn test_move_ferry_2() {
+        let input = "F10\nN3\nF7\nR90\nF11";
+        let instructions: Vec<Instruction2> = input.lines().map(|s| s.parse().unwrap()).collect();
+
+        let mut ferry = new_ferry("E");
+
+        for i in instructions {
+            ferry.travel_2(i);
+        }
+
+        assert_eq!(286, ferry.calc_manhattan_distance());
     }
 }
--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -3,24 +3,39 @@ use std::io::{BufWriter, stdout};
 
 use ferris_says::say;
 
-use crate::ferry::{Instruction, new_ferry};
+use crate::ferry::{Instruction1, Instruction2, new_ferry};
 
 mod ferry;
 
 fn main() {
-    let instructions = read_input();
-
-    let mut ferry = new_ferry(&String::from("E"));
-
-    for instr in instructions {
-        ferry.travel(instr);
+    let instructions_1 = read_input_1();
+    let mut ferry_1 = new_ferry(&String::from("E"));
+    for instr in instructions_1 {
+        ferry_1.travel_1(instr);
     }
 
-    let manhattan_dist = ferry.calc_manhattan_distance();
-    announce_answer(format!("The ferry is at a Manhattan Distance of {}", manhattan_dist));
+    let manhattan_dist_1 = ferry_1.calc_manhattan_distance();
+    announce_answer(format!("The ferry is at a Manhattan Distance of {}", manhattan_dist_1));
+
+    let instructions_2 = read_input_2();
+    let mut ferry_2 = new_ferry(&String::from("E"));
+    for instr in instructions_2 {
+        ferry_2.travel_2(instr);
+    }
+
+    let manhattan_dist_2 = ferry_2.calc_manhattan_distance();
+    announce_answer(format!("The ferry is at a Manhattan Distance of {}", manhattan_dist_2));
 }
 
-fn read_input() -> Vec<Instruction> {
+fn read_input_1() -> Vec<Instruction1> {
+    fs::read_to_string("input.txt")
+        .expect("Error reading file")
+        .lines()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn read_input_2() -> Vec<Instruction2> {
     fs::read_to_string("input.txt")
         .expect("Error reading file")
         .lines()
